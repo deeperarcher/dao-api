d42be714667cf10ccec2ba12a34ec8128ee185fa
AlanLeePhilly <alanjlee89@gmail.com>
2021-04-03 13:46:13 -0500

    ADD: Arrest props

--- test/mocks/Arrest.js
+++ test/mocks/Arrest.js
@@ -6,10 +6,12 @@ export default class Arrest {
   constructor() {
     this.date = formatDate(faker.date.recent());
     this.district = faker.random.number(25).toString();
+    this.isActiveAtTimeOfArrest = faker.random.boolean();
     this.officers = intoArray(3, () =>
       (faker.random.number(89999) + 10000).toString()
     );
 
+    this.priorArrestCount = faker.random.number(5);
     this.referralDate = formatDate(faker.date.recent());
     this.SID = (faker.random.number(89999) + 10000).toString();
   }
